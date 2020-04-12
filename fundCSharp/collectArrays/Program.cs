2f14b1c7eb4b76cfe6130d8d31a129b7dddf4155
sjoseph1848 <sjoseph1848@gmail.com>
2020-04-11 19:52:51 -0500

    Collection deep dive

---
+++ fundCSharp/collectArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace collectArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            // List all of the days in the week 
+            // foreach (string day in daysOfWeek)
+            // {
+            //     Console.WriteLine(day);
+            // }
+            System.Console.WriteLine("Which day do you want to display?");
+            Console.Write("Monday = 1, etc. > ");
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay];
+            System.Console.WriteLine($"That day is {chosenDay}");
+        }
+    }
+}
