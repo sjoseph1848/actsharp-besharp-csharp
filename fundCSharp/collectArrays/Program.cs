b2232a3eb5b68814fef0760b5447d16f1a383316
sjoseph1848 <sjoseph1848@gmail.com>
2020-04-13 19:25:03 -0500

    reading in a csv file to app

--- fundCSharp/collectArrays/Program.cs
+++ fundCSharp/collectArrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace collectArrays
 {
@@ -6,27 +7,17 @@ namespace collectArrays
     {
         static void Main(string[] args)
         {
-            string[] daysOfWeek = {
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-                "Sunday"
-            };
-
-            // List all of the days in the week 
-            // foreach (string day in daysOfWeek)
-            // {
-            //     Console.WriteLine(day);
-            // }
-            System.Console.WriteLine("Which day do you want to display?");
-            Console.Write("Monday = 1, etc. > ");
-            int iDay = int.Parse(Console.ReadLine());
-
-            string chosenDay = daysOfWeek[iDay];
-            System.Console.WriteLine($"That day is {chosenDay}");
+            
+            string filePath = @"/Users/samueljoseph/Desktop/PopbyLargestFinal.csv";
+            
+            CsvReader reader = new CsvReader(filePath);
+            
+            Country[] countries = reader.ReadFirstNCountries(10);
+           
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
         }
     }
 }
